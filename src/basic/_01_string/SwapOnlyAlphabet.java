9195549e8c6d592df0f032a31be3bfb8c35b04d7
chandler <parksm0529@gmail.com>
2024-09-06 00:49:00 +0900

    sol) 선 예외 처리, 후 핵심 로직 구현 - 예외처리: 특수문자는 건너뛴다 - 핵심로직: 알파벳끼리 교환한다
    + String.valueOf / Arrays.toString 차이 구분
    - String.valueOf : 직접 문자열로 반환 ("abc")
    - Arrays.toString : 배열 구조를 문자열로 감싸서 반환 "['a', 'b', 'c']"
    
    Signed-off-by: chandler <parksm0529@gmail.com>

--- src/basic/_01_string/SwapOnlyAlphabet.java
+++ src/basic/_01_string/SwapOnlyAlphabet.java
@@ -8,7 +8,28 @@ public class SwapOnlyAlphabet {
         String inputString = sc.nextLine();
 
         SwapOnlyAlphabet swapOnlyAlphabet = new SwapOnlyAlphabet();
-        System.out.println(swapOnlyAlphabet.solution(inputString));
+        System.out.println(swapOnlyAlphabet.solution2(inputString));
+    }
+
+    private String solution2(String inputString) {
+        char[] array = inputString.toCharArray();
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            if (!Character.isAlphabetic(array[left])) {
+                left++;
+            } else if (!Character.isAlphabetic(array[right])) {
+                right--;
+            } else {
+                char tmp = array[left];
+                array[left] = array[right];
+                array[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return String.valueOf(array);
     }
 
     private String solution(String inputString) {
@@ -26,6 +47,7 @@ public class SwapOnlyAlphabet {
             left++;
             right--;
         }
+
         return String.valueOf(array);
     }
 }
