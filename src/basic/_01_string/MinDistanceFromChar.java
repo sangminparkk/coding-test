cfba2339ccc395bb73f210f8bfb19e185804ad8e
chandler <parksm0529@gmail.com>
2024-09-10 12:57:10 +0900

    sol) left/right indexing process
    - 왼쪽/오른쪽 시작하면서 "min.dis" 갱신
    - 최소값 비교 (Math.min(a,b))
    
    Signed-off-by: chandler <parksm0529@gmail.com>

--- src/basic/_01_string/MinDistanceFromChar.java
+++ src/basic/_01_string/MinDistanceFromChar.java
@@ -11,8 +11,36 @@ public class MinDistanceFromChar {
         char inputChar = scanner.next().charAt(0);
 
         MinDistanceFromChar minDistance = new MinDistanceFromChar();
-        String result = minDistance.solution(inputStr, inputChar);
-        System.out.println(result);
+        for (int x : minDistance.solution2(inputStr, inputChar)) {
+            System.out.print(x + " ");
+        }
+    }
+
+    private int[] solution2(String inputStr, char inputChar) {
+        int[] answer = new int[inputStr.length()];
+        int p = 1000;
+        for (int i = 0; i < inputStr.length(); i++) {
+            if (inputStr.charAt(i) == inputChar) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+
+        p = 1000;
+        for (int i = inputStr.length()-1; i >=0; i--) {
+            if (inputStr.charAt(i) == inputChar) {
+                p = 0;
+            } else {
+                p++;
+                //비교
+                answer[i] = Math.min(answer[i], p); // Math 함수 사용 (if 처리 X)
+            }
+        }
+
+        return answer;
     }
 
     private String solution(String inputStr, char inputChar) {
