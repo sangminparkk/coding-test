d1ea86f2c0962bb3f6a7621f0f94c3dc071ae982
chandler <parksm0529@gmail.com>
2024-09-15 14:27:40 +0900

    refactoring) condition is applied
    Signed-off-by: chandler <parksm0529@gmail.com>

--- src/basic/_02_Array/FibonacciSequence.java
+++ src/basic/_02_Array/FibonacciSequence.java
@@ -8,9 +8,25 @@ public class FibonacciSequence {
         int counter = scanner.nextInt();
 
         FibonacciSequence fibonacciSequence = new FibonacciSequence();
-        for (int number : fibonacciSequence.solution(counter)) {
+        for (int number : fibonacciSequence.solution2(counter)) {
             System.out.print(number + " ");
         }
+    }
+
+    private Integer[] solution2(int counter) {
+        Integer[] numbers = new Integer[counter];
+
+        int previous = 1;
+        int current = 1;
+        numbers[0] = previous;
+        numbers[1] = current;
+
+        for (int i = 2; i < counter; i++) {
+            numbers[i] = previous + current;
+            previous = current;
+            current = numbers[i];
+        }
+        return numbers;
     }
 
     private Integer[] solution(int counter) {
