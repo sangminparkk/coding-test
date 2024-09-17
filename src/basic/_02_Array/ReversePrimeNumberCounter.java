d749f38cb72ff273e04d95b54ec4dee3d3eb8dd5
chandler <parksm0529@gmail.com>
2024-09-17 13:08:49 +0900

    sol) FAIL - Concept X / code Implementation X
    necessary
    1) division : %, /
    2) rev concept : stack (*10 + division) + while loop until reverse number is found
    3) isPrime
    
    Signed-off-by: chandler <parksm0529@gmail.com>

--- src/basic/_02_Array/ReversePrimeNumberCounter.java
+++ src/basic/_02_Array/ReversePrimeNumberCounter.java
@@ -26,8 +26,35 @@ public class ReversePrimeNumberCounter {
         //9
         //32 55 62 20 250 370 200 30 100
 
+        // 321
+        for (int i = 0; i < counter; i++) {
 
+            int tmp = arr[i];
+            int rev = 0;
+            while (tmp != 0) {
+                int t = tmp % 10;
+                rev = rev * 10 + t;
+                tmp = tmp / 10;
+            }
 
-        return null;
+            if (isPrime(rev)) {
+                answer.add(rev);
+            }
+        }
+        return answer;
+    }
+
+    private boolean isPrime(int rev) {
+        if (rev == 1) {
+            return false;
+        }
+
+        for (int i = 2; i < rev; i++) {
+            if (rev % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
