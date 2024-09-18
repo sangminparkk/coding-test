dfb8cb6011d92ebb9551088e99edc133eb6bebba
chandler <parksm0529@gmail.com>
2024-09-18 14:35:38 +0900

    점수 계산 - FAIL
    채점 결과 : 1/5
    
    Signed-off-by: chandler <parksm0529@gmail.com>

---
+++ src/basic/_02_Array/TotalScore.java
@@ -0,0 +1,45 @@
+package basic._02_Array;
+
+import java.util.*;
+
+public class TotalScore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = scanner.nextInt();
+
+        int[] test = new int[counter];
+        for (int i = 0; i < counter; i++) {
+            test[i] = scanner.nextInt();
+        }
+
+        TotalScore totalScore = new TotalScore();
+        System.out.println(totalScore.solution(counter, test));
+    }
+
+    private int solution(int counter, int[] test) {
+        int[] grades = new int[counter];
+
+        // 초기값설정
+        if (test[0] == 0) {
+            grades[0] = 0;
+        } else {
+            grades[0] = 1;
+        }
+
+        for (int i = 1; i < counter-1; i++) {
+            // 틀린경우
+            if (test[i-1] > test[i]) {
+                grades[i] = 0;
+            } else if (test[i-1] < test[i]) {
+                grades[i] = 1;
+            } else if (test[i - 1] == test[i] && test[i] == 0) {
+                grades[i] = 0;
+            } else {
+                grades[i] = grades[i - 1] + 1;
+            }
+        }
+
+
+        return Arrays.stream(grades).sum();
+    }
+}
