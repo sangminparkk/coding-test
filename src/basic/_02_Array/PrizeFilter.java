41b19e28c292f4ce1752fbf753518cda217364ce
chandler <parksm0529@gmail.com>
2024-09-19 17:14:14 +0900

    등수계산 - FAIL
    Problems
    1. easy to find out the two things like max, min.
    2. but how do I find another things like 2nd, 3rd, 4th
    3. Is it possible to use for loop, just one time?
    
    Signed-off-by: chandler <parksm0529@gmail.com>

---
+++ src/basic/_02_Array/PrizeFilter.java
@@ -0,0 +1,40 @@
+package basic._02_Array;
+
+import java.util.Scanner;
+
+public class PrizeFilter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int counter = scanner.nextInt();
+
+        int[] grades = new int[counter];
+        for (int i = 0; i < counter; i++) {
+            grades[i] = scanner.nextInt();
+        }
+
+        PrizeFilter prizeFilter = new PrizeFilter();
+
+        for (int prize : prizeFilter.solution(counter, grades)) {
+            System.out.print(prize + " ");
+        }
+    }
+
+    private int[] solution(int counter, int[] grades) {
+        int[] answer = new int[counter];
+//        5
+//        87 89 92 100 76
+        // 예외처리 : 즉 가장 높은 점수가 92점인데 92점이 3명 존재하면 1등이 3명이고 그 다음 학생은 4등이 된다.
+        // answer index = grades index
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < counter; i++) {
+            if (grades[i] > max) {
+                max = grades[i];
+                answer[i] = 1;
+            }
+        }
+
+        return answer;
+    }
+
+}
