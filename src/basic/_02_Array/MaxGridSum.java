18cc0916f52f8715ecb3c225746abb0996ba5886
chandler <parksm0529@gmail.com>
2024-09-22 23:34:27 +0900

    sol) minimize variables / refactor  loops for a better readability
    Signed-off-by: chandler <parksm0529@gmail.com>

--- src/basic/_02_Array/MaxGridSum.java
+++ src/basic/_02_Array/MaxGridSum.java
@@ -15,8 +15,34 @@ public class MaxGridSum {
         }
 
         MaxGridSum maxGridSum = new MaxGridSum();
-        int max = maxGridSum.solution(counter, grid);
+        int max = maxGridSum.solution2(counter, grid);
         System.out.println(max);
+    }
+
+    private int solution2(int counter, int[][] grid) {
+        int answer = Integer.MIN_VALUE; // max
+        int sum1, sum2;
+
+        // row vs column
+        for (int i = 0; i < counter; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < counter; j++) {
+                sum1 += grid[i][j];
+                sum2 += grid[j][i];
+            }
+            answer = Math.max(answer, sum1);
+            answer = Math.max(answer, sum2);
+        }
+
+        // diagonals
+        sum1 = sum2 = 0;
+        for (int i = 0; i < counter; i++) {
+            sum1 += grid[i][i];
+            sum2 += grid[i][counter-1-i];
+        }
+        answer = Math.max(answer, sum1);
+        answer = Math.max(answer, sum2);
+        return answer;
     }
 
     private int solution(int counter, int[][] grid) {
