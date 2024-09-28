2448e21626a65bc65c149663f51a237e400b7fbb
chandler <parksm0529@gmail.com>
2024-09-28 23:50:40 +0900

    sol) two points algorithm from lecture.
    1. Make a pointer for each array.
    - Using the index of array, pointer can move like index if it's plus
    2. Use a while loop until all elements has been searched.
    3. If one array is done, need one more step, another also have to be searched.
    4. At this time, we don't know about which array is done. So check the both of them.
    
    Signed-off-by: chandler <parksm0529@gmail.com>

--- src/basic/_03_TwoPoints/AddArrays.java
+++ src/basic/_03_TwoPoints/AddArrays.java
@@ -20,8 +20,38 @@ public class AddArrays {
         }
 
         AddArrays addArrays = new AddArrays();
-        addArrays.solution(arr1, arr2).forEach(i -> System.out.print(i + " "));
+//        addArrays.solution(arr1, arr2).forEach(i -> System.out.print(i + " "));
+        addArrays.solution2(arr1, arr2).forEach(i -> System.out.print(i + " "));
     }
+
+    private List<Integer> solution2(int[] arr1, int[] arr2) {
+        /**
+         * how to solve the way of O(n) without two nested for loop
+         * > I did not think about the while loop at all. it's not comfortable for me.
+         */
+
+        List<Integer> answers = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < arr1.length && p2 < arr2.length) {
+            if (arr1[p1] < arr2[p2]) {
+                answers.add(arr1[p1++]);
+            } else {
+                answers.add(arr2[p2++]);
+            }
+        }
+
+        // 누가 남았는지 모르니까 둘다 해야된다? ㄷㄷ
+        while (p1 < arr1.length) {
+            answers.add(arr1[p1++]);
+        }
+
+        while (p2 < arr2.length) {
+            answers.add(arr2[p2++]);
+        }
+
+        return answers;
+    }
+
 
     private List<Integer> solution(int[] arr1, int[] arr2) {
         List<Integer> answers = new ArrayList<>();
