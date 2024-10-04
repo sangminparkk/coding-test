99f465677c0b1f51f3d4896fdf568086de0afd0d
chandler <parksm0529@gmail.com>
2024-10-04 23:12:32 +0900

    sol) need to study while loop + solved from the lecture
    Signed-off-by: chandler <parksm0529@gmail.com>

--- src/basic/_03_TwoPoints/FindIntersection.java
+++ src/basic/_03_TwoPoints/FindIntersection.java
@@ -1,5 +1,8 @@
 package basic._03_TwoPoints;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class FindIntersection {
@@ -20,10 +23,33 @@ public class FindIntersection {
         }
 
         FindIntersection findIntersection = new FindIntersection();
-        for (int num : findIntersection.solution(num1, num2, arr1, arr2)) {
+        for (int num : findIntersection.solution2(num1, num2, arr1, arr2)) {
             System.out.print(num + " ");
         }
 
+    }
+
+    private List<Integer> solution2(int num1, int num2, int[] arr1, int[] arr2) {
+        List<Integer> answer = new ArrayList<>();
+        // 전처리
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        int p1 = 0, p2 = 0;
+
+        while (p1 < num1 && p2 < num2) {
+            if (arr1[p1] == arr2[p2]) {
+                answer.add(arr1[p1++]);
+//                p1++;
+                p2++;
+            } else if (arr1[p1] < arr2[p2]) {
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+
+        return answer;
     }
 
     private int[] solution(int num1, int num2, int[] arr1, int[] arr2) {
