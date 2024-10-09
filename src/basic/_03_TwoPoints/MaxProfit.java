9edb2c667308afdf7ee69369a3ea84150ff407da
chandler <parksm0529@gmail.com>
2024-10-09 23:58:36 +0900

    최대매출 : FAIL
    - sliding window 이론을 몰라서 강의 힌트를 듣고 푸는데도 틀렸음
    
    Signed-off-by: chandler <parksm0529@gmail.com>

---
+++ src/basic/_03_TwoPoints/MaxProfit.java
@@ -0,0 +1,40 @@
+package basic._03_TwoPoints;
+
+import java.util.Scanner;
+
+public class MaxProfit {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        MaxProfit maxProfit = new MaxProfit();
+        System.out.println(maxProfit.solution(arr, k));
+
+    }
+
+    private int solution(int[] arr, int k) {
+        int max;
+
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        max = sum;
+        for (int i = k; i < arr.length; i++) {
+            if (sum + arr[i] - arr[i - k] > max) {
+                sum = sum + arr[i] - arr[i - k];
+                max = sum;
+            }
+        }
+
+        return max;
+    }
+}
